2f4acd77fc32ab9cd98a6ca66c8da6ddaf5fb8c5
Mari Farid <farid.mari3@etu.univ-lorraine.fr>
2023-12-20 17:24:59 +0100

    Modification de Tache suite à la modification de Composite

--- Sources/S3D_SAE_Trello/src/main/java/com/example/s3d_sae_trello/Tache.java
+++ Sources/S3D_SAE_Trello/src/main/java/com/example/s3d_sae_trello/Tache.java
@@ -3,22 +3,18 @@ package com.example.s3d_sae_trello;
 // Tache classique qui ne possède pas de sous taches
 
 public class Tache extends CompositeTache {
-
-    public Tache(int id, String n, int urgence, int tempsEstime) {
-        super(id, n, urgence, tempsEstime);
-    }
-
-    public Tache(int id, String n, int urgence, int tempsEstime, String description) {
-        super(id, n, urgence, tempsEstime, description);
-    }
-    @Override
-    public void ajouterSousTache(CompositeTache t) {
-
+    public Tache(int id, String nomTache, String nomUtilisateur, String prenomUtilisateur, int urgence, int tempsEstime, String description) {
+        super(id, nomTache, nomUtilisateur, prenomUtilisateur, urgence, tempsEstime, description);
     }
 
     @Override
-    public void retirerSousTache(int id) {
+    public boolean ajouterSousTache(CompositeTache t) {
+        return false;
+    }
 
+    @Override
+    public boolean retirerSousTache(int id) {
+        return false;
     }
 
     @Override
