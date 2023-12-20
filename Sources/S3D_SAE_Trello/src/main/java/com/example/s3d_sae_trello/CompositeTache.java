84046b2779785514e99fb1757accea074dea98d0
Mari Farid <farid.mari3@etu.univ-lorraine.fr>
2023-12-20 17:20:59 +0100

    Modification du Composite niveau constructeur + ajout de nouvelles methodes

--- Sources/S3D_SAE_Trello/src/main/java/com/example/s3d_sae_trello/CompositeTache.java
+++ Sources/S3D_SAE_Trello/src/main/java/com/example/s3d_sae_trello/CompositeTache.java
@@ -1,50 +1,39 @@
 package com.example.s3d_sae_trello;
 
-
-
 import java.time.LocalDate;
 import java.util.Date;
 
 public abstract class CompositeTache {
     protected int id;
     private String nom;
-
-    private String descript;
+    private String nomUser;
+    private String prenomUser;
+    private String description;
     private LocalDate date;
     private int degreUrgence;
     private int tempsEstime;
     private boolean tacheRealise;
-    private LocalDate dateDebutReal;
+    private Date dateDebutReal;
     private Dependance dependance; // Contient dépendance Mere et Fille de la tâche
 
 
-    public CompositeTache(int id, String n, int urgence, int tempsEstime) {
+    public CompositeTache(int id, String nomTache, String nomUtilisateur, String prenomUtilisateur, int urgence, int tempsEstime, String description) {
         this.id = id;
-        this.nom = n;
+        this.nom = nomTache;
+        this.nomUser = nomUtilisateur;
+        this.prenomUser = prenomUtilisateur;
+        this.description = description;
         this.date = LocalDate.now();
         this.degreUrgence = urgence;
         this.tempsEstime = tempsEstime;
         this.tacheRealise = false;
         this.dateDebutReal = null;
         this.dependance = new Dependance();
-        this.descript = "";
     }
 
-    public CompositeTache(int id, String n, int urgence, int tempsEstime, String descr) {
-        this.id = id;
-        this.nom = n;
-        this.date = LocalDate.now();
-        this.degreUrgence = urgence;
-        this.tempsEstime = tempsEstime;
-        this.tacheRealise = false;
-        this.dateDebutReal = null;
-        this.dependance = new Dependance();
-        this.descript = descr;
-    }
+    public abstract boolean ajouterSousTache(CompositeTache t);
 
-    public abstract void ajouterSousTache(CompositeTache t);
-
-    public abstract void retirerSousTache(int id);
+    public abstract boolean retirerSousTache(int id);
 
     public int getId() {
         return id;
@@ -70,10 +59,25 @@ public abstract class CompositeTache {
         return tacheRealise;
     }
 
-    public LocalDate getDateDebutReal() {
+    public Date getDateDebutReal() {
         return dateDebutReal;
     }
 
+    public String getNomUser() {
+        return nomUser;
+    }
+
+    public String getPrenomUser() {
+        return prenomUser;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void ajouterDescription(String d){
+        description += d;
+    }
 
     /**
      * Récupère les dépendances de la classe
@@ -105,4 +109,30 @@ public abstract class CompositeTache {
     public String  afficherDependance(){
         return this.dependance.afficherDependance(this);
     }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Tache numero : ").append(id).append(", d'urgence : ").append(degreUrgence).append("\n");
+        builder.append("Nom : ").append(nom).append("\n");
+        builder.append("Cree par ").append(nomUser).append(" ").append(prenomUser).append(" le ").append(date).append("\n");
+        builder.append("Description : ").append(description).append("\n");
+        builder.append("Temps estime : ").append(tempsEstime).append("\n");
+        builder.append("-----------------------------------\n");
+
+        if (dependance != null) {
+            builder.append(dependance.afficherDependance(this));
+        }
+
+        builder.append("\n");
+
+        if (tacheRealise) {
+            builder.append("\n~ Tache realisee ~ \n");
+        } else {
+            builder.append("\n~ Tache " + nom + " non realisee ~\n");
+        }
+
+        return builder.toString();
+    }
+
 }
