f050713f41f71cd902cfbff8b6b02c8ae75c768d
Alann <alann.monnier-blondeau9@etu.univ-lorraine.fr>
2024-01-10 21:16:34 +0100

    Nouvelles modifications Gantt

--- Sources/S3D_SAE_Trello/src/main/java/com/example/s3d_sae_trello/GanttFX.java
+++ Sources/S3D_SAE_Trello/src/main/java/com/example/s3d_sae_trello/GanttFX.java
@@ -19,7 +19,7 @@ public class GanttFX extends HBox{
 
     private ModeleMenu modeleMenu;
     private int x1 = 200;
-    private int y1 = 200;
+    private int y1 = 250;
     private final double largueur = Screen.getPrimary().getBounds().getWidth();
     private final double longueur = Screen.getPrimary().getBounds().getHeight();
     private Canvas canvas = new Canvas(largueur, longueur);
@@ -27,7 +27,7 @@ public class GanttFX extends HBox{
     public GanttFX(ModeleMenu modele) throws IOException {
         this.modeleMenu = modele;
 
-
+        /**
         ArrayList<Tache> meretache1 = new ArrayList<>();
         meretache1.add(modele.getColonneLignes().get(0).getTache("Tache 3"));
 
@@ -39,7 +39,7 @@ public class GanttFX extends HBox{
         // Ajouter des dépendances
         modeleMenu.ajouterDependance(modele.getColonneLignes().get(0).getTache("Tache 0"), meretache1, "mere");
         modeleMenu.ajouterDependance(modele.getColonneLignes().get(0).getTache("Tache 3"), meretache2, "mere");
-
+         */
 
         // Récupère les taches les plus au fond
         ArrayList<Tache> m = modeleMenu.recupererTacheFinal();
@@ -48,17 +48,28 @@ public class GanttFX extends HBox{
         LocalDate dateDebut = tachesmere.get(0).get(0).getDateDebutReal();
         LocalDate dateFin = tachesmere.get(tachesmere.size()-1).get(tachesmere.get(tachesmere.size()-1).size()-1).getDateDebutReal();
 
+        int longueurTache = 60;
+        int hauteurTache = 40;
 
         // Création de la barre de temps
-        gc.fillText(String.valueOf(dateDebut), x1, 18, x1+25*tachesmere.size());
-        gc.strokeLine(x1, 10, x1+25*tachesmere.size(), 10);
+        int ecart = x1;
+        System.out.println(tachesmere.size());
+        int decalage = 0;
+        for (int nb=0; nb<tachesmere.size(); nb++){
+            LocalDate date = tachesmere.get(nb).get(0).getDateDebutReal();
+            gc.fillText(String.valueOf(date), ecart, hauteurTache+5+decalage, x1+longueurTache+5*tachesmere.size());
+            gc.strokeLine(ecart, 18, ecart, 15);
+            ecart += longueurTache+5;
+            decalage+=2;
+        }
+        gc.strokeLine(x1, 18, x1+(longueurTache+5)*tachesmere.size(), 18);
 
         // Gestion des coordonnées pour créer les liens sous forme de ligne
         Map<Coordonnees, Tache> coords = new HashMap<>();
         for (int i = tachesmere.size()-1 ; i > -1; i--){
             for (Tache tt : tachesmere.get(i)){
                 if(modeleMenu.getDependance().containsKey(tt)){
-                    coords.put(new Coordonnees(x1+50, y1+10), tt);
+                    coords.put(new Coordonnees(x1+longueurTache, y1+10), tt);
                 }
                 for (Coordonnees coordonnees : coords.keySet()){
                     if(modeleMenu.getDependance().get(coords.get(coordonnees)).contains(tt)){
@@ -67,12 +78,12 @@ public class GanttFX extends HBox{
                         break;
                     }
                 }
-                gc.strokeRect(x1, y1, 50, 20);
+                gc.strokeRect(x1, y1, longueurTache, hauteurTache);
                 gc.fillText(tt.getNom(),x1+6, y1+15);
-                y1+= 25;
+                y1+= hauteurTache+5;
             }
-            x1 += 55;
-            y1 -= 25 + (25/tachesmere.size());
+            x1 += longueurTache+5;
+            y1 -= hauteurTache+5 + (hauteurTache+5/tachesmere.size());
         }
 
         this.getChildren().addAll(canvas);
